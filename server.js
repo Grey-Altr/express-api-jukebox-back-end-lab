e28d94adfdebdc2c325338223c0cb329cc34d60d
GreyA <grey.altaer@gmail.com>
2025-03-11 21:03:58 -0700

    init commit: dependencies installed :: dir init: models; controllers

---
+++ server.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const methodOverride = require('method-override');
+const cors = require('cors');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.json());
+app.use(logger("dev"));
+app.use(methodOverride('_method'));
+app.use(cors({ origin: 'http://localhost:5173' }));
+
+// Routes go here
+
+app.listen(3000, () => {
+  console.log("The express app is ready!");
+});
