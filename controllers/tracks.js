f04f3dbffffae21544a2ea977e19660cc9449cee
GreyA <grey.altaer@gmail.com>
2025-03-11 21:31:40 -0700

    POST build: tracks.js; test passed

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -2,6 +2,13 @@ const Track = require('../models/track.js');
 const express = require('express');
 const router = express.Router();
 
-
+router.post ('/', async (req, res) => {
+    try {
+        const createdTrack = await Track.create(req.body);
+        res.status(201).json(createdTrack);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    };
+});
 
 module.exports = router;
